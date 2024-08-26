7db52bd0b89eac54e4c322378ae86d2110454145
sik2 <bickwnag123@gmail.com>
2024-08-26 21:32:11 +0900

    이전페이지과 다음페이지

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # React + Vite Pokemon App
 
+- [x] 이전페이지과 다음페이지
 - [ ] 이미지 불러오기
 - [ ] 컴포넌트 최적화 작업 useMemo
 - [ ] 컴포넌트 최적화 작업 useCallback
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useState, useEffect } from "react";
 function App() {
-  return <>hi</>;
+  const [pokemons, setPokemons] = useState([]);
+  const [loding, setLoding] = useState(true);
+  const [offset, setOffset] = useState(0);
+  const totalCount = 1302;
+  const limit = 30;
+
+  const showPrev = () => {
+    setOffset(offset - limit);
+  };
+
+  const showNext = () => {
+    setOffset(offset + limit);
+  };
+
+  useEffect(() => {
+    setLoding(true);
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemons(data.results);
+        setLoding(false);
+      });
+  }, [offset]);
+
+  if (loding) {
+    return "로딩 중...";
+  }
+
+  return (
+    <>
+      <h1>포켓몬 도감</h1>
+      offset: {offset}
+      {offset > 0 && <button onClick={showPrev}>이전</button>}
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+      </ul>
+      {totalCount > offset + limit && <button onClick={showNext}>다음</button>}
+    </>
+  );
 }
 
 export default App;
