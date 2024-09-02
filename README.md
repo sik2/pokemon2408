afeb97e5f90223dc3ad5bd99f3fd15f6bc63044b
sik2 <bickwang123@gmail.com>
2024-09-02 20:47:30 +0900

    컴포넌트 최적화 작업 useMemo

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
   - https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/1.png
 - [x] 이미지 불러오기 리펙토링
 - [x] 이미지 불러오기 리펙토링2
-- [ ] 컴포넌트 최적화 작업 useMemo
+- [x] 컴포넌트 최적화 작업 useMemo
 - [ ] 컴포넌트 최적화 작업 useCallback
 - [ ] 의존모듈 설치 React-Router list 라우팅 처리
 - [ ] 상세페이지 구현
-- [ ] 테일윈드 도입 디자인 개선
+- [ ] 테일윈드 CDN 도입 디자인 개선
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [loding, setLoding] = useState(true);
   const [offset, setOffset] = useState(0);
+  const [count, setCount] = useState(0);
+
   const totalCount = 1302;
   const limit = 30;
 
@@ -16,19 +18,25 @@ function App() {
 
   const getNumberFromUrl = (url) => {
     const urlList = url.split("/");
-    console.log(urlList);
     return parseInt(urlList[urlList.length - 2]);
   };
 
-  const forPrintPokemons = pokemons.map((pokemon) => {
-    const imgNumber = getNumberFromUrl(pokemon.url);
-    const imgUrl = `https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/${imgNumber}.png`;
-    return {
-      imgNumber,
-      imgUrl,
-      ...pokemon,
-    };
-  });
+  let i = 0;
+  const forPrintPokemons = useMemo(
+    () =>
+      pokemons.map((pokemon) => {
+        console.log("hit", ++i);
+
+        const imgNumber = getNumberFromUrl(pokemon.url);
+        const imgUrl = `https://cdn.jsdelivr.net/gh/PokeAPI/sprites/sprites/pokemon/${imgNumber}.png`;
+        return {
+          imgNumber,
+          imgUrl,
+          ...pokemon,
+        };
+      }),
+    [pokemons]
+  );
 
   useEffect(() => {
     setLoding(true);
@@ -47,6 +55,8 @@ function App() {
   return (
     <>
       <h1>포켓몬 도감</h1>
+      {/* 카운트가 변경되면 forPrintPokemons 가 계속 실행되지만 useMemo 를 통해 필요할때만 수정되도록 할 수 있다 */}
+      count: <button onClick={() => setCount(count + 1)}>{count}</button>
       offset: {offset}
       {offset > 0 && <button onClick={showPrev}>이전</button>}
       <ul>
