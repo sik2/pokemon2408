4345c89f250d34be3422d94e7ec95c600d1d4e7c
sik2 <bickwang123@gmail.com>
2024-09-09 20:30:33 +0900

    리스트 페이지 디자인 개선

--- src/pages/PokemonListPage.jsx
+++ src/pages/PokemonListPage.jsx
@@ -8,7 +8,7 @@ function PokemonListPage() {
   const [count, setCount] = useState(0);
 
   const totalCount = 1302;
-  const limit = 30;
+  const limit = 8;
 
   const showPrev = useCallback(() => {
     setOffset(offset - limit);
@@ -22,7 +22,7 @@ function PokemonListPage() {
     const urlList = url.split("/");
     return parseInt(urlList[urlList.length - 2]);
   };
-
+  //카운트가 변경되면 forPrintPokemons 가 계속 실행되지만 useMemo 를 통해 필요할때만 수정되도록 할 수 있다
   let i = 0;
   const forPrintPokemons = useMemo(
     () =>
@@ -56,32 +56,41 @@ function PokemonListPage() {
   return (
     <>
       <LayoutPage>
-        <h1>포켓몬 도감 리스트</h1>
-        {/* 카운트가 변경되면 forPrintPokemons 가 계속 실행되지만 useMemo 를 통해 필요할때만 수정되도록 할 수 있다 */}
-        count: <button onClick={() => setCount(count + 1)}>{count}</button>
-        offset: {offset}
-        {offset > 0 && <button onClick={showPrev}>이전</button>}
-        <ul>
-          {forPrintPokemons.map((pokemon, index) => (
-            <li
-              key={index}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                borderButtom: "1px solid black",
-              }}
-            >
-              <span>{pokemon.imgNumber}</span>
-              <img src={pokemon.imgUrl} alt="포켓몬이미지" />
-              <Link to={`/pokemons/${pokemon.imgNumber}`}>
-                <span>{pokemon.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        {totalCount > offset + limit && (
-          <button onClick={showNext}>다음</button>
-        )}
+        <div className="m-2">
+          <div className="badge badge-success">{offset}</div>
+          {offset > 0 && (
+            <button className="btn btn-sm btn-success" onClick={showPrev}>
+              이전
+            </button>
+          )}
+          {totalCount > offset + limit && (
+            <button className="btn btn-sm btn-success" onClick={showNext}>
+              다음
+            </button>
+          )}
+        </div>
+        <hr />
+        <div className="con-1 w-full h-screen max-w-7xl max-auto">
+          <h1 className="header text-2xl">포켓몬 도감 리스트</h1>
+          <ul className="flex flex-wrap gap-4 mt-2">
+            {forPrintPokemons.map((pokemon, index) => (
+              <li
+                key={index}
+                className="card card-compact w-72 bg-base-100 shadow-xl"
+              >
+                <div className="card-body">
+                  <div className="badge badge-primary badge-outline">
+                    {pokemon.imgNumber}
+                  </div>
+                  <img src={pokemon.imgUrl} alt="포켓몬이미지" />
+                  <Link to={`/pokemons/${pokemon.imgNumber}`}>
+                    <div className="font-bold">{pokemon.name}</div>
+                  </Link>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </LayoutPage>
     </>
   );
